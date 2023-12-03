a5685ea03767ce46288951509f0af91c182bc717
MateusMayan <mateusmayan@icloud.com>
2023-12-03 20:06:46 -0300

    Adicionado LocalStorage de Tarefas

--- script.js
+++ script.js
@@ -1,31 +1,60 @@
 // Elementos
 
-const input = document.querySelector("#inputTxt")
-const list = document.querySelector(".list")
-let control = 0 // variável de controle do atributo #id da <li>
-
+const input = document.querySelector("#inputTxt");
+const list = document.querySelector(".list");
+let control = 0; // variável de controle do atributo #id da <li>
+var arr = []; // Array Para LocalStorage
 //Funções
+
 keyUp = (e) => {
   if (e.key === "Enter") {
-    const li = document.createElement("li")
-    li.innerHTML = input.value
-    li.classList.add(`itemList${control}`)
-    li.setAttribute("onclick", `deletar('itemList${control}')`)
+    const li = document.createElement("li");
+    li.innerHTML = input.value;
+    li.classList.add(`itemList${control}`);
+    li.setAttribute("onclick", `completar('itemList${control}')`);
     list.appendChild(li);
     control++;
+    input.value = "";
+    //Local Storage Add
+    if (localStorage.meuArr) {
+      arr = JSON.parse(localStorage.getItem("meuArr")); // Converte para array a string 
+    }
+    arr.push(li.innerHTML);
+    localStorage.meuArr = JSON.stringify(arr); //Transforma minha array em String
   }
-}
+};
 
-deletar = (id) => {
-  const remove = document.querySelector(`.${id}`)
-  const confirma = confirm("Quer apagar esse elemento?")
+completar = (id) => {
+  const confirma = confirm("Você completou essa tarefa?");
   if (confirma) {
-    remove.remove(remove)
+    document.body.querySelector(`.${id}`).style.textDecoration = "line-through";
   } else {
-    alert("Item mantido")
+    alert("Item não concluído");
+    document.body.querySelector(`.${id}`).style.textDecoration = "none";
   }
-}
+};
+
+exibirTarefasSalvas = () => {
+  list.innerHTML = "";
+  if (localStorage.meuArr) {
+    arr = JSON.parse(localStorage.getItem("meuArr"));
+  }
+  for (var i in arr) {
+    let li = document.createElement("li");
+    li.innerHTML = arr[i];
+    list.append(li);
+    li.classList.add(`itemList${control}`);
+    li.setAttribute("onclick", `completar('itemList${control}')`);
+  }
+};
+
+limparTarefasSalvas = () => {
+  arr = [];
+  localStorage.meuArr = JSON.stringify(arr);
+  list.innerHTML = "";
+  control++;
+};
 
 //Eventos
 
-input.addEventListener("keyup", keyUp)
+input.addEventListener("keyup", keyUp);
